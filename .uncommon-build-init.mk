92d9d3653e443479f97f89e5c7132bf63fc5964a
James Cuzella <james.cuzella@lyraphase.com>
2022-04-22 03:11:43 -0600

    Fixing more command stringing in uncommon-build bootstrapper

--- .uncommon-build-init.mk
+++ .uncommon-build-init.mk
@@ -1,5 +1,5 @@
 ifeq (,$(wildcard .uncommon-build))
-  $(shell                                                                                        \
+  $(shell echo "\033[44m🔍\033[0m \033[0;36m... Looking for .uncommon-build ... \033[0m" >&2;    \
     echo "\033[0;31m.uncommon-build is curiously missing... 🧐 installing submodule\033[0m" >&2; \
     git submodule add https://github.com/LyraPhase/uncommon-build.git .uncommon-build >&2;       \
     echo "\033[0;31mCommitting '.uncommon-build' and '.gitmodules'\033[0m" >&2;                  \
