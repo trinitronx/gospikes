488e48040d338699a87a690eacd3254775f91e73
James Cuzella <james.cuzella@lyraphase.com>
2022-04-28 00:41:50 -0600

    Remove build-aux / uncommon-build Prototype files

--- build-aux/main.mk
+++
@@ -1,6 +0,0 @@
-# Include everything else
-include $(build_aux)/platform-detect.mk
-include $(build_aux)/multi-platform.mk
-include $(build_aux)/lint.mk
-include $(build_aux)/test.mk
-include $(build_aux)/help.mk
